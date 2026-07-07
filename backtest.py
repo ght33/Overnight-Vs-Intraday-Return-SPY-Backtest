0701c131a93f713c0866af421fb2e8bb67ea5030
ght33 <giacomotrenouth2@gmail.com>
2026-07-07 20:59:34 +0000

    feat: implement data download engine

--- backtest.py
+++ backtest.py
@@ -2,3 +2,41 @@ import yfinance as yf
 import pandas as pd 
 
 def download_data(ticker, start_date, end_date):
+    """
+    Pulling historical price data for SPY
+    - Need daily Open, High, Low, and Close prices to be able
+    to compute difference between overnight and intraday price
+    movement
+    """
+    print(f'Fetching historical data for {ticker} from {start_date} to {end_date}...')
+    data = yf.download(ticker, start=start_date, end=end_date)
+    return data
+
+if __name__ == '__main__':
+    # Defining real variables
+    TICKER = 'SPY'
+    START = '2020-01-01'
+    END = '2026-01-01'
+
+    # Calling the function and saving to df
+    df = download_data(TICKER, START, END)
+
+    # Printing the first 5 rows of the dataframe to verify data
+    print(df.head())
+
+def calculate_returns(data):
+    """
+    Calculate intraday and overnight return streams using pandas.
+    Intraday: (Close - Open) / Open
+    Overnight: (Current Open - Previous Close) / Previous Close
+    """
+    df = data.copy()
+
+    # Intraday: Buy open, sell close of that same day
+    df['Intraday_Return'] = (df['Close'] - df['Open']) / df['Open']
+
+    # Overnight: Buy close of previous day, sell open of current day
+    #.shift(1) grabs close price from row above (previous day)
+    df['Overnight_Return'] = (df['Open'] - df['Close'].shift(1)) / df['Close'].shift(1)
+    df = drop.na()
+    return df
